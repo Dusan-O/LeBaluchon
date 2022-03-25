29212b15921fd778a62bdb9dcb9e788a127b4af1
Dusan-O <dusan.orescanin@gmail.com>
2022-03-25 13:15:13 +0100

    Creation Main, LauncScreen & Asset files & UIViewcontrollerAndALert file Add icons to the app & launchScreen app view

--- LeBaluchon/Source/UIViewcontrollerAndAlert.swift
+++ LeBaluchon/Source/UIViewcontrollerAndAlert.swift
@@ -5,4 +5,15 @@
 //  Created by Dusan Orescanin on 24/03/2022.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func presentUIAlert(message: String) {
+        
+        let alertVC = UIAlertController(title: "Erreur", message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    
+}

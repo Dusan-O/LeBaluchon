1927cf3437a122f281885f94709c7eb5ad95146a
Dusan-O <dusan.orescanin@gmail.com>
2022-03-28 12:28:47 +0200

    Creation CommonProtocols.swift/copies URLSession's dataTask function signature for testing purposes

--- LeBaluchon/Source/CommonProtocols.swift
+++ LeBaluchon/Source/CommonProtocols.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+// Auxiliar protocol that copies URLSession's dataTask function signature for testing purposes(to define mock types that conform to this protocol)
+protocol RequestInterface {
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: RequestInterface {}
